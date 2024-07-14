a0cf22b75a49926613e90d7edbb8bccf7d344514
MikWink <winklermi76145@th-nuernberg.de>
2024-07-14 22:29:13 +0200

    FTR: Implemented make_trace and add_trace function for map_generatorv2

--- map_generatorv2.py
+++ map_generatorv2.py
@@ -31,20 +31,14 @@ class Map:
         # Create a meshgrid from x and y coordinates
         X, Y = np.meshgrid(x_coords, y_coords)
 
-        layout = go.Layout(
-            margin=dict(l=0, r=0, b=0, t=20)
-        )
-
-        # Create a 3D surface plot
+        # Create a 3D surface plot with layout and scene updates
         self.fig = go.Figure(
-            data=[go.Surface(x=X, y=Y, z=self.offseted_height_map, colorscale='Viridis', showscale=False, name='Terrain',
-                             hovertemplate='<b>Lat:</b> %{y}<br>' +
-                                           '<b>Long:</b> %{x}<br>' +
-                                           '<b>H:</b> %{z}<extra></extra>')],
-            layout=layout)
-        # Set the title and axis labels
-        self.fig.update_layout(scene=dict(xaxis_title='Long', yaxis_title='Lat', zaxis_title='Height'))
-        self.fig.update_scenes(aspectmode='manual', aspectratio=dict(x=1, y=2, z=0.1))
+            data=[],
+            layout=go.Layout(margin=dict(l=0, r=0, b=0, t=20),
+                             scene=dict(xaxis_title='Long', yaxis_title='Lat', zaxis_title='Height',
+                                        aspectmode='manual', aspectratio=dict(x=1, y=2, z=0.1)))
+        )
+        self.fig.add_trace(self.make_trace([X, Y, self.offseted_height_map], type='surface'))
 
         # Generate the HTML string of the figure
         html_string = pyo.plot(self.fig, include_plotlyjs='cdn', output_type='div')
@@ -58,15 +52,30 @@ class Map:
         with open('terrain_map.html', 'w') as file:
             file.write(html_string)
 
-    def add_trace(self, points, uid=None):
-        x = [float(point[1]) for point in points]
-        y = [float(point[0]) for point in points]
-        z = [float(point[2]) for point in points]
+    def add_trace(self, trace, uid=None):
+        self.fig.add_trace(trace)
+        if uid is not None:
+            self.fig.data[-1].uid = uid
 
-        self.fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode='markers', name='BS+MS',
-                                        marker=dict(size=3, color='red', opacity=0.8, symbol='x')))
+    def make_trace(self, points, type='scatter', color='black', size=3):
+
+        if type == 'scatter':
+            x = [float(point[1]) for point in points]
+            y = [float(point[0]) for point in points]
+            z = [float(point[2]) for point in points]
+            return go.Scatter3d(x=x, y=y, z=z, mode='markers', name='BS+MS',
+                                        marker=dict(size=size, color=color, opacity=0.8, symbol='x'))
+        elif type == 'surface':
+            x = points[0]
+            y = points[1]
+            z = points[2]
+            return go.Surface(x=x, y=y, z=z, colorscale='Viridis', showscale=False, name='Terrain',
+                             hovertemplate='<b>Lat:</b> %{y}<br>' +
+                                           '<b>Long:</b> %{x}<br>' +
+                                           '<b>H:</b> %{z}<extra></extra>')
 
 
 map = Map()
-map.add_trace([['50.673881', '10.953427', '521.5'], ['50.674918', '10.906670', '531.5'], ['50.704759', '10.919444', '530.7'], ['50.682106', '10.933819', '700'], ['50.686887', '10.936072', '630']], 'bs+ms')
+map.add_trace(map.make_trace([['50.673881', '10.953427', '521.5'], ['50.674918', '10.906670', '531.5'], ['50.704759', '10.919444', '530.7'], ['50.682106', '10.933819', '700'], ['50.686887', '10.936072', '630']]))
+map.add_trace(go.Scatter3d(x=[10.953427], y=[50.673881], z=[521.5], mode='markers', name='MS'))
 map.update()
